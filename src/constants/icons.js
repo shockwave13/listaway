c3d94942547931ca984b54d167b8cc775e429345
nero <artur.duchenko@gmail.com>
2019-11-21 18:21:19 +0200

    Add logo, change name

---
+++ src/constants/icons.js
@@ -0,0 +1,4 @@
+const icons = {
+  logo: require('../assets/icons/logo.png'),
+};
+export default icons;
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,6 @@ import colors from './colors';
 import images from './images';
 import fonts from './fonts';
 import globalStyles from './styles';
+import icons from './icons';
 
-export {colors, images, fonts, globalStyles};
+export {colors, images, fonts, globalStyles, icons};
